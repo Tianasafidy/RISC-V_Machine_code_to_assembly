847ff3e6cbf9980a6dcad9b803eb556394e4e2fc
Safidy Ratsimatahomanana <tratsima-23@telecom-paris.fr>
2024-12-08 16:08:46 +0100

    move normal_register and declare it as static variable

--- machine_to_asm.c
+++ machine_to_asm.c
@@ -3,6 +3,39 @@
 #include <stdio.h>
 #include <string.h>
 
+// array of normal registers (without floating point registers) 
+static const char * normal_register[32] = {"zero", 
+				 "ra",
+				 "sp", 
+				 "gp", 
+				 "tp", 	
+				 "t0", 	
+				 "t1", 
+				 "t2", 
+				 "s0/fp", 
+				 "s1", 
+				 "a0", 
+				 "a1", 
+				 "a2", 
+				 "a3", 
+				 "a4", 
+				 "a5", 
+				 "a6", 
+				 "a7", 
+				 "s2", 
+				 "s3", 
+				 "s4", 
+				 "s5", 
+				 "s6", 
+				 "s7", 
+				 "s8", 
+				 "s9", 
+				 "s10", 
+				 "s11", 
+				 "t3", 
+				 "t4", 
+				 "t5", 
+				 "t6"};
 
 uint32_t get_rd_reg(uint32_t hex_code){
 	static uint8_t _rd_reg; 
