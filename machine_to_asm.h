5e4804e7c55f6cd1d5f38b84114b533e578950ec
Safidy Ratsimatahomanana <tratsima-23@telecom-paris.fr>
2024-12-08 16:08:54 +0100

    move normal_register and declare it as static variable

--- machine_to_asm.h
+++ machine_to_asm.h
@@ -16,39 +16,7 @@
 #define OPCODE_U_Type2  (0b0110111) //lui
 #define OPCODE_UJ_Type (0b1101111) //jal
 
-// array of normal registers (without floating point registers) 
-const char * normal_register[32] = {"zero", 
-				 "ra",
-				 "sp", 
-				 "gp", 
-				 "tp", 	
-				 "t0", 	
-				 "t1", 
-				 "t2", 
-				 "s0/fp", 
-				 "s1", 
-				 "a0", 
-				 "a1", 
-				 "a2", 
-				 "a3", 
-				 "a4", 
-				 "a5", 
-				 "a6", 
-				 "a7", 
-				 "s2", 
-				 "s3", 
-				 "s4", 
-				 "s5", 
-				 "s6", 
-				 "s7", 
-				 "s8", 
-				 "s9", 
-				 "s10", 
-				 "s11", 
-				 "t3", 
-				 "t4", 
-				 "t5", 
-				 "t6"};  
+  
 
 uint32_t get_rd_reg(uint32_t hex_code);
 
